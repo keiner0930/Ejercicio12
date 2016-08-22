d05e64933c186b3e8204f74ff8a49f24b4547753
“keiner0930” <keiner.arrieta0930@hotmail.com>
2016-08-22 18:01:52 -0500

    Interfaz Grafica Creada

--- src/Interfaz/Principal12.java
+++ src/Interfaz/Principal12.java
@@ -28,24 +28,77 @@ public class Principal12 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtDias = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtPeliculas = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        txtMonto = new javax.swing.JTextField();
+        cmdCalcular = new javax.swing.JButton();
+        cmdBorrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jLabel1.setFont(new java.awt.Font("Traditional Arabic", 3, 36)); // NOI18N
+        jLabel1.setText("Alquiler de DVD");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 280, -1));
+
+        jLabel2.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel2.setText("Peliculas Alquiladas");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 120, 140, -1));
+        jPanel1.add(txtDias, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 50, 30));
+
+        jLabel3.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel3.setText("Numero de Dias de Alquiler");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, 210, -1));
+
+        txtPeliculas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPeliculasActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtPeliculas, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 110, 50, 30));
+
+        jLabel4.setFont(new java.awt.Font("Traditional Arabic", 2, 18)); // NOI18N
+        jLabel4.setText("Monto a Pagar");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 170, 110, -1));
+        jPanel1.add(txtMonto, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 200, 80, 30));
+
+        cmdCalcular.setText("Calcular");
+        cmdCalcular.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdCalcularActionPerformed(evt);
+            }
+        });
+        jPanel1.add(cmdCalcular, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 250, -1, -1));
+
+        cmdBorrar.setText("Borrar");
+        jPanel1.add(cmdBorrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 250, -1, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 345, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtPeliculasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPeliculasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtPeliculasActionPerformed
+
+    private void cmdCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdCalcularActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmdCalcularActionPerformed
 
     /**
      * @param args the command line arguments
@@ -83,6 +136,15 @@ public class Principal12 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdBorrar;
+    private javax.swing.JButton cmdCalcular;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtDias;
+    private javax.swing.JTextField txtMonto;
+    private javax.swing.JTextField txtPeliculas;
     // End of variables declaration//GEN-END:variables
 }
